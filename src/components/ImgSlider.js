b4a623bb5a16efc75fb3c5fcd2bbaa677f743aa0
AlaaXDz <sifodil@yandex.com>
2022-09-19 22:40:37 +0100

    ADD: Image slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -28,14 +28,37 @@ function ImgSlider() {
 export default ImgSlider
 
 const Carousel = styled(Slider)`
-  margin-top: 20px;
+max-width: 90vw;
+margin: 20px auto 0px auto; 
+ul li button {
+  &:before{
+    font-size: 10px;
+    color: rgb(150,158,171);
+  }
+}
+li.slick-active button:before {
+  color: white;
+}
+
+.slick-list{
+  overflow: visible;
+}
+button {
+  z-index: 1;
+}
 `
 const Wrap = styled.div`
   img{
+    cursor: pointer;
+    border: 4px solid transparent;
     border-radius: 4px;
     width: 100%;
     height: 100%;
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
     rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition-duration: 300ms;
+    &:hover {
+      border: 4px solid rgba(249,249,249, 0.8);
+    }
   }
 `
