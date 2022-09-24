0398d38c04aaa161caefc07fc079367c7e1fec50
AlaaXDz <sifodil@yandex.com>
2022-09-24 22:44:46 +0100

    Detail page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Routes, //instead of Switch
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <Header />
-        <Home />
+        <Routes >
+          <Route path="/detail" element={<Detail />} />
+            
+          <Route path="/" element={<Home />} />
+            
+        </Routes>
+      </Router>
     </div>
   );
 }
